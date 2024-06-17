3a8e3e83d0e353b4937e3135e5d7ac3956b49117
aa-kr <aa-kr@mail.ru>
2024-06-17 12:11:48 +0300

    add foto

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load(" img/")
+icon = pygame.image.load(" img/igratir.png")
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/target.png")
@@ -28,7 +28,7 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.get_pos()
-            if target_x, < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
+            if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDHT - target_wight)
                 target_y = random.randint(0,SCREEN_HEIGHT - target_height)
     screen.blit(target_img, (target_x, target_y))
