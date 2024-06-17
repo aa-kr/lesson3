9f03f60117dadc01cd93c6c155c3c00af07c03c6
aa-kr <aa-kr@mail.ru>
2024-06-17 11:43:57 +0300

    add file

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+import random
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load(" img/")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("img/target.png")
+target_wight = 80
+target_height = 80
+
+target_x = random.randint(0, SCREEN_WIDTH - target_wight)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255),random.randint(0, 255), random.randint(0, 255))
+
+
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.get_pos()
+            if target_x, < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDHT - target_wight)
+                target_y = random.randint(0,SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
+
+pygame.guit()
