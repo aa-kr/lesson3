1abeaf04ab62d912b15cf365d5a7d237e479bee0
aa-kr <aa-kr@mail.ru>
2024-06-17 12:42:47 +0300

    add ispravlenie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+
 pygame.init()
 
 SCREEN_WIDTH = 800
@@ -7,18 +8,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load(" img/igratir.png")
+icon = pygame.image.load("img/igratir.png")
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/target.png")
-target_wight = 80
+target_width = 80
 target_height = 80
 
-target_x = random.randint(0, SCREEN_WIDTH - target_wight)
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255),random.randint(0, 255), random.randint(0, 255))
-
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
@@ -27,11 +27,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.get_pos()
-            if target_x < mouse_x < target_x + target_widht and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDHT - target_wight)
-                target_y = random.randint(0,SCREEN_HEIGHT - target_height)
-    screen.blit(target_img, (target_x, target_y))
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
     pygame.display.update()
 
-pygame.guit()
+pygame.quit()
